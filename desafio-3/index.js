28f88bc6f0c69a97fc2d2b24e7bf6d436f5bae50
Lucas Zanin <lukinhas.so.lokura@hotmail.com>
2022-03-02 22:12:53 -0300

    add to the file desafio-3

---
+++ desafio-3/index.js
@@ -0,0 +1,7 @@
+var mensagem = confirm('Olá Seja Bem Vindo(a), Aventureiro(a)! Gostaria de pagar R$50,00 para passar a noite na nossa hospedagem?')
+
+if(mensagem == true){
+    alert('"Ótimo! Nós temos as melhores camas de toda a região!')
+}else{
+    alert(' "Que pena! Você parecia ser uma pessoa mais legal')
+}
