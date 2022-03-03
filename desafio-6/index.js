fdf65ac5c0f6adef33b2fa91a7e2be831914db24
Lucas Zanin <lukinhas.so.lokura@hotmail.com>
2022-03-02 22:34:29 -0300

    adicionando desafio 6

---
+++ desafio-6/index.js
@@ -0,0 +1,25 @@
+function soma(num1,num2){
+    let soma = num1+num2
+    return soma
+}
+
+function subtrai(num1,num2){
+    let subtrai = num1 - num2
+    return subtrai
+}
+
+function divide(num1,num2){
+    let divide = num1 / num2
+    return divide
+}
+
+
+function multiplica(num1,num2){
+    let multiplica = num1 * num2
+    return multiplica
+}
+
+console.log('A soma dos números é '+soma(10,5))
+console.log('A subtração dos números é '+subtrai(10,5))
+console.log('A Divisão dos números é '+divide(10,5))
+console.log('A Multiplicação dos números é '+multiplica(10,5))
